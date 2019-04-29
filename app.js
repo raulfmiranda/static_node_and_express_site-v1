b954a0a60a9f8683b6a1412967c60164ff8098be
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-04-29 16:04:58 -0300

    Done until Using Pug in Your Express App

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Express!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log("This application is running on localhost: 3000!");
+});

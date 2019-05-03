ba30ccaaa08cf6b7f7e4dac615eb88acb99674d9
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-05-03 11:44:33 -0300

    Done project

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use((req, res, next) => {
     next(err);
 });
 
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
 app.listen(3000, () => {
     console.log("This application is running on localhost: 3000!");
 });
